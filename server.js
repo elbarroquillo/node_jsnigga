1ce33d25e0147996a30982b6e8f54022e8d00dd6
elbarroquillo <elbarroquillo@paterostechnologicalcollege.edu.ph>
2025-02-27 23:22:53 +0800

    BAKA ETO NA YON

--- server.js
+++ server.js
@@ -7,6 +7,19 @@ const PORT = 8080;
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, "public")));
 
+// Dynamic route to serve HTML files based on filename
+app.get("/:filename", (req, res) => {
+  const { filename } = req.params;
+
+  // Only allow page1.html to page5.html
+  if (!/^page[1-5]\.html$/.test(filename)) {
+    return res.status(404).send("Page not found");
+  }
+
+  // Serve the requested HTML file
+  res.sendFile(path.join(__dirname, "public", filename));
+});
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
